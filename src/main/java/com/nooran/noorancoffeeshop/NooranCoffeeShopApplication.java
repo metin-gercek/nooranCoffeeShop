465c8ac463ff577007408e26232c5a41cc49cf69
metin-gercek <metin.gercek@edu.bc.fi>
2022-02-05 23:31:08 +0200

    start project

---
+++ src/main/java/com/nooran/noorancoffeeshop/NooranCoffeeShopApplication.java
@@ -0,0 +1,13 @@
+package com.nooran.noorancoffeeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NooranCoffeeShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NooranCoffeeShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nooran/noorancoffeeshop/NooranCoffeeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nooran.noorancoffeeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NooranCoffeeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
